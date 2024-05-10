dd2a233db03fb9c8ecec7b6006e0ced271e0ef97
thegypsyboyo <lewisonyango9@gmail.com>
2024-05-10 20:57:22 +0300

    made type changes

--- pages/api/auth/lib/mongodb.ts
+++ pages/api/auth/lib/mongodb.ts
@@ -1,16 +1,19 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable import/no-mutable-exports */
-// This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URL;
+declare const global: {
+    _mongoClientPromise?: Promise<MongoClient>;
+};
+
+const uri: string | undefined = process.env.MONGODB_URL;
 const options = {
     useUnifiedTopology: true,
     useNewUrlParser: true,
 };
 
-let client;
-let clientPromise;
+let client: MongoClient | undefined;
+let clientPromise: Promise<MongoClient> | undefined;
 
 if (!process.env.MONGODB_URL) {
     throw new Error("Please add your Mongo URI to .env.local");
@@ -20,13 +23,13 @@ if (process.env.NODE_ENV === "development") {
     // In development mode, use a global variable so that the value
     // is preserved across module reloads caused by HMR (Hot Module Replacement).
     if (!global._mongoClientPromise) {
-        client = new MongoClient(uri, options);
+        client = new MongoClient(uri!, options as any);
         global._mongoClientPromise = client.connect();
     }
     clientPromise = global._mongoClientPromise;
 } else {
     // In production mode, it's best to not use a global variable.
-    client = new MongoClient(uri, options);
+    client = new MongoClient(uri!, options as any);
     clientPromise = client.connect();
 }
 
